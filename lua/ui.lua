19a9bce719598f1f589c32e10976bcb1feb8711d
Olivier Martin <olivier.martin@govirtuo.com>
2021-10-15 18:39:26 +0200

    nvim tree conf and colorscheme

--- lua/ui.lua
+++ lua/ui.lua
@@ -1,7 +1,9 @@
-vim.cmd 'colorscheme base16-default-dark'
+--vim.cmd 'colorscheme base16-default-dark'
+vim.cmd 'colorscheme base16-nord'
+
 vim.cmd [[
 autocmd BufEnter * hi Search guifg=#e3e3e3 guibg=#585858
 autocmd BufEnter * hi IncSearch guifg=#e3e3e3 guibg=#585858
 autocmd BufEnter * hi IndentBlanklineChar guifg=#2a2a2a
+autocmd BufEnter * hi Normal guibg=#181818'
 ]]
--- hi Normal guibg=#181818
