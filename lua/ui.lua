059cda3fadffcc8decda6447e8bc35c271876fba
olive <olive.martin@gmail.com>
2021-10-16 10:41:35 +0200

    LineNr and Gutter bg

--- lua/ui.lua
+++ lua/ui.lua
@@ -5,5 +5,7 @@ vim.cmd [[
 autocmd BufEnter * hi Search guifg=#e3e3e3 guibg=#585858
 autocmd BufEnter * hi IncSearch guifg=#e3e3e3 guibg=#585858
 autocmd BufEnter * hi IndentBlanklineChar guifg=#2a2a2a
-autocmd BufEnter * hi Normal guibg=#181818'
+autocmd BufEnter * hi Normal guibg=#181818
+autocmd BufEnter * hi LineNr guibg=#181818
+autocmd BufEnter * hi SignColumn guibg=#181818
 ]]
