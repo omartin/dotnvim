94324d743e488ab428ca1387a5e31a0edf29e26f
Olivier Martin <olivier.martin@govirtuo.com>
2022-09-25 14:09:25 +0200

    last ones

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -90,6 +90,7 @@ return packer.startup(function(use)
   -- lsp
   use "neovim/nvim-lspconfig" -- enable LSP
   use "williamboman/nvim-lsp-installer" -- simple to use language server installer
+  --use "williamboman/mason" -- SEE LATER, replaces the above ^
   use "tamago324/nlsp-settings.nvim" -- language server settings defined in json for
   use "jose-elias-alvarez/null-ls.nvim" -- for formatters and linters
   use "hrsh7th/nvim-cmp" -- The completion plugin
